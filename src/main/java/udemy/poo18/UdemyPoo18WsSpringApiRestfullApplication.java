e85fc07d8f98b2c543c713020f92fc002f66b695
MatosOliveira <matosalem.oliveira@gmail.com>
2023-07-11 15:13:56 -0300

    Primeiro commit - projeto criado

---
+++ src/main/java/udemy/poo18/UdemyPoo18WsSpringApiRestfullApplication.java
@@ -0,0 +1,13 @@
+package udemy.poo18;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UdemyPoo18WsSpringApiRestfullApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UdemyPoo18WsSpringApiRestfullApplication.class, args);
+	}
+
+}
---
+++ src/test/java/udemy/poo18/UdemyPoo18WsSpringApiRestfullApplicationTests.java
@@ -0,0 +1,13 @@
+package udemy.poo18;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UdemyPoo18WsSpringApiRestfullApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
