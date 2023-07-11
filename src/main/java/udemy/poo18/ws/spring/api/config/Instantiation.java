94b7140c695bf3c393bf38b86f7279919adfc123
MatosOliveira <matosalem.oliveira@gmail.com>
2023-07-11 16:23:55 -0300

    Operação de instanciação da base de dados

---
+++ src/main/java/udemy/poo18/ws/spring/api/config/Instantiation.java
@@ -0,0 +1,31 @@
+package udemy.poo18.ws.spring.api.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import udemy.poo18.ws.spring.api.domain.User;
+import udemy.poo18.ws.spring.api.repositories.UserRepository;
+
+@Configuration
+public class Instantiation implements CommandLineRunner {
+
+	@Autowired
+	UserRepository userRepository;
+	
+	@Override
+	public void run(String... args) throws Exception {
+		
+		userRepository.deleteAll();
+		
+		User maria = new User(null, "Maria Brown", "maria@gmail.com");
+		User alex = new User(null, "Alex Green", "alex@gmail.com");
+		User bob = new User(null, "Bob Grey", "bob@gmail.com");
+		
+		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+		
+	}
+
+}
