3b4fbb59da4eefd24e22f90c69f509f403cba32c
MatosOliveira <matosalem.oliveira@gmail.com>
2023-07-11 23:55:01 -0300

    Inserção de usuário com POST

--- src/main/java/udemy/poo18/ws/spring/api/services/UserService.java
+++ src/main/java/udemy/poo18/ws/spring/api/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import udemy.poo18.ws.spring.api.domain.User;
+import udemy.poo18.ws.spring.api.dto.UserDTO;
 import udemy.poo18.ws.spring.api.repositories.UserRepository;
 import udemy.poo18.ws.spring.api.services.exception.ObjectNotFoundException;
 
@@ -23,5 +24,13 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado."));
+	}
+	
+	public User insert(User obj) {
+		return repository.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO userDto) {
+		return new User(userDto.getId(), userDto.getName(), userDto.getEmail());
 	}
 }
