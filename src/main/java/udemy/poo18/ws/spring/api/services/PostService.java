3c6d1cccaf455f7c0ff63561a0cb830e1414b91b
MatosOliveira <matosalem.oliveira@gmail.com>
2023-07-13 12:55:59 -0300

    Obtendo um post por id

---
+++ src/main/java/udemy/poo18/ws/spring/api/services/PostService.java
@@ -0,0 +1,23 @@
+package udemy.poo18.ws.spring.api.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import udemy.poo18.ws.spring.api.domain.Post;
+import udemy.poo18.ws.spring.api.repositories.PostRepository;
+import udemy.poo18.ws.spring.api.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repository;
+
+	public Post findById(String id) {
+		Optional<Post> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado."));
+	}
+
+}
